919764cf035de3c75bd6f44965f59163f0522c5a
sancarbar <sancabar@gmail.com>
2023-01-23 23:19:59 -0500

    1. Added unit tests for Codelab Part 1: Intro to Spring Boot with Java-

---
+++ src/main/java/org/adaschool/tasksplanner/controller/HealthController.java
@@ -0,0 +1,8 @@
+package org.adaschool.tasksplanner.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+}
---
+++ src/test/java/org/adaschool/tasksplanner/HealthControllerTest.java
@@ -0,0 +1,34 @@
+package org.adaschool.tasksplanner;
+
+import org.adaschool.tasksplanner.controller.HealthController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+@SpringBootTest
+public class HealthControllerTest {
+
+    @Autowired
+    private HealthController controller;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void testGetHealthController() throws Exception {
+        mockMvc.perform(get("/health"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("API working OK!"));
+    }
+}
