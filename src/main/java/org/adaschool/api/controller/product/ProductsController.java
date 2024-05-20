f755c96e576e39ca02fba46952b4c107e0655d31
sebalooc35 <andres.ordonez@galileo.edu>
2024-05-19 20:28:50 -0600

    add swagger with api information

--- src/main/java/org/adaschool/api/controller/product/ProductsController.java
+++ src/main/java/org/adaschool/api/controller/product/ProductsController.java
@@ -1,5 +1,8 @@
 package org.adaschool.api.controller.product;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.adaschool.api.exception.ProductNotFoundException;
 import org.adaschool.api.repository.product.Product;
 import org.adaschool.api.repository.product.ProductDto;
@@ -15,6 +18,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/products/")
+@Api(value = "Controlador de productos", description = "Controla todos los endpoints para manejar diferentes productos")
 public class ProductsController {
 
     private final ProductsService productsService;
@@ -24,27 +28,35 @@ public class ProductsController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> createProduct(@RequestBody ProductDto productDto) {
+    @ApiOperation(value = "agrega un producto a la base de datos", response = Product.class)
+    public ResponseEntity<Product> createProduct(
+            @ApiParam(value = "cuerpo con los datos del producto a agregar", required = true) @RequestBody ProductDto productDto) {
         Product product = this.productsService.save(new Product(productDto));
         URI createdProductUri = URI.create(product.getId());
         return ResponseEntity.created(createdProductUri).body(product);
     }
 
     @GetMapping
+    @ApiOperation(value = "devuelve todos los productos registrados en la base de datos")
     public ResponseEntity<List<Product>> getAllProducts() {
         List<Product> products = this.productsService.all();
         return ResponseEntity.ok(products);
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<Product> findById(@PathVariable("id") String id) {
+    @ApiOperation(value = "devuelve (si existe) el producto con el id indicado", response = Product.class)
+    public ResponseEntity<Product> findById(
+            @ApiParam(value = "id del producto a buscar", required = true) @PathVariable("id") String id) {
         Optional<Product> optionalProduct = this.productsService.findById(id);
         if (optionalProduct.isEmpty()) throw new ProductNotFoundException(id);
         return ResponseEntity.ok(optionalProduct.get());
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<Product> updateProduct(@PathVariable String id,@RequestBody ProductDto productDto) {
+    @ApiOperation(value = "edita (si existe) el producto con id indicado con los datos enviados", response = Product.class)
+    public ResponseEntity<Product> updateProduct(
+            @ApiParam(value = "id del producto a actualizar", required = true) @PathVariable String id,
+            @ApiParam(value = "datos a modificar del producto", required = true) @RequestBody ProductDto productDto) {
         Optional<Product> optionalProduct = this.productsService.findById(id);
         if (optionalProduct.isEmpty()) throw new ProductNotFoundException(id);
         Product product = optionalProduct.get();
@@ -54,7 +66,9 @@ public class ProductsController {
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable String id) {
+    @ApiOperation(value = "borra (si existe) el producto con el id indicado", response = Product.class)
+    public ResponseEntity<Void> deleteProduct(
+            @ApiParam(value = "id del producto a borrar", required = true) @PathVariable String id) {
         if (this.productsService.findById(id).isEmpty()) throw new ProductNotFoundException(id);
         this.productsService.deleteById(id);
         return ResponseEntity.ok(null);
--- src/main/java/org/adaschool/api/controller/user/UsersController.java
+++ src/main/java/org/adaschool/api/controller/user/UsersController.java
@@ -1,5 +1,8 @@
 package org.adaschool.api.controller.user;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.adaschool.api.exception.UserNotFoundException;
 import org.adaschool.api.repository.user.User;
 import org.adaschool.api.repository.user.UserDto;
@@ -14,6 +17,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/users/")
+@Api(value = "controlador de los endpoints para modificar los usuarios", description = "contiene el CRUD para la tabla usuarios")
 public class UsersController {
 
     private final UsersService usersService;
@@ -23,26 +27,34 @@ public class UsersController {
     }
 
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody UserDto userDto) {
+    @ApiOperation(value = "crea un nuevo usuario", response = User.class)
+    public ResponseEntity<User> createUser(
+            @ApiParam(value = "informacion del cliente a registrar", required = true) @RequestBody UserDto userDto) {
         User user = this.usersService.save(new User(userDto));
         return ResponseEntity.created(null).body(user);
     }
 
     @GetMapping
+    @ApiOperation(value = "devuelve todos los clientes registrados en la base de datos", response = List.class)
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = this.usersService.all();
         return ResponseEntity.ok(users);
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<User> findById(@PathVariable("id") String id) {
+    @ApiOperation(value = "devuelve el cliente (si existe) con el id indicado", response = List.class)
+    public ResponseEntity<User> findById(
+            @ApiParam(value = "id del cliente a buscar", required = true) @PathVariable("id") String id) {
         Optional<User> optionalUser = this.usersService.findById(id);
         if (optionalUser.isEmpty()) throw new UserNotFoundException(id);
         return ResponseEntity.ok(optionalUser.get());
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<User> updateUser(@PathVariable String id, @RequestBody UserDto userDto) {
+    @ApiOperation(value = "edita (si existe) el usuario con el id indicado con la informacion adjunta", response = User.class)
+    public ResponseEntity<User> updateUser(
+            @ApiParam(value = "id del usuario a editar", required = true) @PathVariable String id,
+            @ApiParam(value = "informacion a editar del usuario", required = true) @RequestBody UserDto userDto) {
         Optional<User> optionalUser = this.usersService.findById(id);
         if (optionalUser.isEmpty()) throw new UserNotFoundException(id);
         User user = optionalUser.get();
@@ -52,7 +64,10 @@ public class UsersController {
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable String id) {
+    @ApiOperation(value = "borra (si existe) el usuario con el id indicado", response = void.class)
+    public ResponseEntity<Void> deleteUser(
+            @ApiParam(value = "id del cliente a borrar", required = true)
+            @PathVariable String id) {
         Optional<User> optionalUser = this.usersService.findById(id);
         if (optionalUser.isEmpty()) throw new UserNotFoundException(id);
         this.usersService.deleteById(id);
